95d49d083374a713daa4189962e72893034ae192
hegi90 <76959740+patrickhegnauer@users.noreply.github.com>
2024-06-14 15:05:21 +0200

    Update test1.js

--- tests/test1.js
+++ tests/test1.js
@@ -1,5 +1,13 @@
 import { Selector, ClientFunction } from 'testcafe';
 
+    // List of URLs to crawl
+    const urls = [
+        'https://www.css.ch/de/privatkunden.html?adb_validation_sessionid=ce2dfadc-2cf7-4d1c-ba6e-623a136c7805',
+        'https://www.css.ch/de/privatkunden/meine-gesundheit/ernaehrung/ernaehrungswissen/zitronenwasser.html?adb_validation_sessionid=ce2dfadc-2cf7-4d1c-ba6e-623a136c7805',
+        'https://www.css.ch/de/privatkunden/schnell-erledigt.html?adb_validation_sessionid=ce2dfadc-2cf7-4d1c-ba6e-623a136c7805',
+        // Add more URLs as needed
+    ];
+
 const setUserAgent = ClientFunction(userAgent => {
     Object.defineProperty(navigator, 'userAgent', {
     value: userAgent,
@@ -7,11 +15,15 @@ const setUserAgent = ClientFunction(userAgent => {
     });
     });
 
-fixture('Getting Started')
-    .page('https://www.css.ch?adb_validation_sessionid=2c8ca2c1-c854-41aa-95ea-6a6887a6935f');
 
-test('My first test', async t => {
-    await setUserAgent('MyCustomUserAgent/1.0');
-    await t
-        .click('#level-one-link-0')
-});
+fixture('Getting Started')
+// Loop through each URL and create a test
+for (const url of urls) {
+    test(`Crawl ${url}`, async t => {
+        //set useragent
+        await setUserAgent('MyCustomUserAgent/1.0');
+        // Navigate to the page
+        await t.navigateTo(url);
+        await t.wait(2000);
+    });
+}
