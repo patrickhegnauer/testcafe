05f5f009e692b8f159322d85974176d64bb689ab
hegi90 <76959740+patrickhegnauer@users.noreply.github.com>
2024-06-14 14:06:29 +0200

    Update test1.js

--- tests/test1.js
+++ tests/test1.js
@@ -1,11 +1,17 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
+
+const setUserAgent = ClientFunction(userAgent => {
+    Object.defineProperty(navigator, 'userAgent', {
+    value: userAgent,
+    writable: true,
+    });
+    });
 
 fixture('Getting Started')
     .page('https://www.css.ch?adb_validation_sessionid=2c8ca2c1-c854-41aa-95ea-6a6887a6935f');
 
 test('My first test', async t => {
+    await setUserAgent('MyCustomUserAgent/1.0');
     await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button')
-        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+        .click('#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll')
 });
